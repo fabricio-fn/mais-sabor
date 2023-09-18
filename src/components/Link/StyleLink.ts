3380409d7d939b338847620ba5d1e0bb04bfd75b
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-09-18 17:54:56 -0300

    add new link style

--- src/components/Link/StyleLink.ts
+++ src/components/Link/StyleLink.ts
@@ -2,35 +2,45 @@ import Link from "next/link";
 import { css, styled } from "styled-components";
 
 interface iLink {
-    linkStyle: "normal" | "light" | "underline"
+	linkStyle: "normal" | "light" | "underline" | "dark"
 }
 
-export const StyleLink = styled(Link)<iLink>`
-    ${({ linkStyle }) => {
-        switch (linkStyle) {
-            case "normal":
-                return css`
-                    color: ${({ theme }) => theme.colors.additional_5};
-                    text-decoration: none;
+export const StyleLink = styled(Link) <iLink>`
+	transition: .4s;
 
-                    &:hover {
-                        color: ${({ theme }) => theme.colors.primary};
-                    }
-                    `
-            case "light":
-                return css`
-                    color: ${({ theme }) => theme.colors.secondary};
-                    text-decoration: none;
-                    font-weight: 400 !important;
+	${({ linkStyle }) => {
+	switch (linkStyle) {
+		case "normal":
+			return css`
+				color: ${({ theme }) => theme.colors.additional_5};
+				text-decoration: none;
 
-                    &:hover {
-                        text-decoration: underline;
-                    }
-                `
-            case "underline":
-                return css`
-                    color: ${({ theme }) => theme.colors.primary};
-                `
-        }
-    }}
+				&:hover {
+					color: ${({ theme }) => theme.colors.primary};
+				}
+			`
+		case "light":
+			return css`
+				color: ${({ theme }) => theme.colors.secondary};
+				text-decoration: none;
+				font-weight: 400 !important;
+
+				&:hover {
+					text-decoration: underline;
+				}
+			`
+		case "underline":
+			return css`
+				color: ${({ theme }) => theme.colors.primary};
+			`
+		case "dark":
+			return css`
+				color: ${({ theme }) => theme.colors.additional_1};
+				text-decoration: none;
+
+				&:hover {
+					color: ${({ theme }) => theme.colors.primary};
+				}
+			`
+	}}}
 `
