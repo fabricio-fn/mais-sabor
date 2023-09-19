84d81fd01c3523c35aac78aae328d2308e57e0ed
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-09-19 11:06:14 -0300

    finished footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import { FlexBoxContainer } from "../FlexBoxContainer/FlexBoxContainer";
+import { StyleLink } from "../Link/StyleLink";
+import { StyleText } from "../Text/StyleText";
+import FooterLinks from "./FooterLinks/FooterLinks";
+import { StyleFooter } from "./StyleFooter";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>© Copyright - Todos os direitos reservados</p>
-    </footer>
+    <>
+      <FooterLinks />
+      <StyleFooter>
+        <StyleText tag="p" fontSize="xxs" color="light">Copyright © 2023 - All Right Reserved</StyleText>
+
+        <FlexBoxContainer>
+          <StyleLink href="/" linkStyle="light">Terms</StyleLink>
+          <StyleLink href="/" linkStyle="light">Privacy</StyleLink>
+          <StyleLink href="/" linkStyle="light">Security</StyleLink>
+        </FlexBoxContainer>
+      </StyleFooter>
+    </>
   )
 }
