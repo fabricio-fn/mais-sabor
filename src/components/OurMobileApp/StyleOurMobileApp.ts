082ed059f0fd98b1f14a8398417cdace06a6098d
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-09-21 08:11:32 -0300

    add StyleOurMenuApp

---
+++ src/components/OurMobileApp/StyleOurMobileApp.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const StyleOurMobileApp = styled.section`
+  justify-content: space-between !important;
+  flex-wrap: wrap;
+  gap: 2rem;
+
+  div {
+    flex-direction: column;
+    align-items: start !important;
+  }
+
+  .img {
+    width: 300px;
+  }
+  
+  .content {
+    width: 430px;
+    gap: 2em;
+
+    .buttons {
+      flex-direction: row;
+      gap: 1rem;
+    }
+    
+    @media (max-width: 469px) {
+      width: 100%;
+
+      .buttons, .buttons > button { width: 100%;}
+      
+      .buttons {flex-direction: column;}
+    }
+  }
+
+  @media (max-width: 952px) {
+    justify-content: center !important;   
+  }
+`
