8806e8203ccbcefd7959b114443d84f379ceada0
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-11-24 16:45:09 -0300

    🗃️ raw: export of components

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+'use client'
+
+export { Badge } from './Badge';
+export { Button } from './Button';
+export { ButtonLink } from './ButtonLink';
+export { Box } from './Box';
+export { Field } from '@/components/Field/Field';
+export { StyleTypography } from '@/components/Typography/StyleTypography';
+export { StyleLink } from '@/components/Link/StyleLink';
+export { Home } from '@/components/Home/Home';
+export { Footer } from '@/components/Footer/Footer';
+export { Header } from '@/components/Header/Header';
+export { PizzaSpecialOffer } from '@/components/PizzaSpecialOffer/PizzaSpecialOffer';
+export { HeaderSignInSignUp } from '@/components/HeaderSignInSignUp/HeaderSignInSignUp';
+export { OurMenu } from '@/components/OurMenu/OurMenu';
+export { Testimonials } from '@/components/Testimonials/Testimonials';
+export { OurBlog } from '@/components/OurBlog/OurBlog';
+export { Newsletter } from '@/components/Newsletter/Newsletter';
+export { OurMobileApp } from '@/components/OurMobileApp/OurMobileApp';
